dfdbf1599dd29f8dc62e01a096a1c526b72c0d82
jtx007 <jamesjacobthomas7@gmail.com>
2019-07-29 20:25:29 -0400

    created a navbar, layout, and three base pages

---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import layoutStyles from './layout.module.css'
+import { Link } from "gatsby"
+
+
+
+export default ({ children }) => (
+    <nav>
+        <Link className={layoutStyles.logoLink} to="/"><img src="https://fontmeme.com/permalink/190730/14e2ec117a477a811d88a66e4ecfe65a.png" alt="pokemon-font" border="0"/></Link>
+        <ul className={layoutStyles.navLinks}>
+            <Link className={layoutStyles.navLink} to="/">Home</Link>
+            <Link className={layoutStyles.navLink} to="/pokemon">Pokemon</Link>
+            <Link className={layoutStyles.navLink} to="/about">About</Link>
+        </ul>
+        {children}
+    </nav>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,9 @@
-import React from "react"
+import React from 'react';
 import Layout from '../components/layout'
+
 
 export default () => (
     <Layout>
-        
-    </Layout>
-    
 
+    </Layout>
 )
